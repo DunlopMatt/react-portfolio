44293fa7236c2ac921bbe20c95a108a6a0eb3ddb
matt dunlop <matthewjdunlop@outlook.com>
2022-04-28 17:18:13 +0100

    Updates

---
+++ precache-manifest.0d797e1cccf3299bbf70a85199507541.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e2c1b8f46e976f5eaa6",
+    "url": "/react-portfolio/static/css/main.38f68a42.chunk.css"
+  },
+  {
+    "revision": "2e2c1b8f46e976f5eaa6",
+    "url": "/react-portfolio/static/js/main.2e2c1b8f.chunk.js"
+  },
+  {
+    "revision": "b3b809bf97541adacdbc",
+    "url": "/react-portfolio/static/js/1.b3b809bf.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "395bfe51b6f258f275a6b7e483ab9366",
+    "url": "/react-portfolio/static/media/Ecomm.395bfe51.PNG"
+  },
+  {
+    "revision": "28be5408d24782d5525ace6652c3874c",
+    "url": "/react-portfolio/static/media/MovieDash.28be5408.PNG"
+  },
+  {
+    "revision": "d7b08c03c100b8422fbbccfb8d0e89cc",
+    "url": "/react-portfolio/static/media/MatterBirds.d7b08c03.PNG"
+  },
+  {
+    "revision": "8d9d7d8edbed12ed9adff470221e5e89",
+    "url": "/react-portfolio/static/media/Fitness.8d9d7d8e.PNG"
+  },
+  {
+    "revision": "dfb51c1e263bf15d74e6edad3f26eda3",
+    "url": "/react-portfolio/static/media/Portfolio.dfb51c1e.PNG"
+  },
+  {
+    "revision": "9ea3047330a1a2e8451bb1c485f9b21f",
+    "url": "/react-portfolio/static/media/w3_html5-ar21.9ea30473.svg"
+  },
+  {
+    "revision": "f7fa700b61bac1654c8bddf469a3af42",
+    "url": "/react-portfolio/static/media/w3_css-ar21.f7fa700b.svg"
+  },
+  {
+    "revision": "f6d0312a0ee9eb54634a026d7b4ab062",
+    "url": "/react-portfolio/static/media/javascript-ar21.f6d0312a.svg"
+  },
+  {
+    "revision": "ddd11c2b829e68ee21b9030bf3cc2727",
+    "url": "/react-portfolio/static/media/reactjs-ar21.ddd11c2b.svg"
+  },
+  {
+    "revision": "613cf57c00db5ba5c15b331620b6376f",
+    "url": "/react-portfolio/static/media/nodejs-ar21.613cf57c.svg"
+  },
+  {
+    "revision": "d571053a3cda81b67bfdaee97caab9bc",
+    "url": "/react-portfolio/static/media/mongodb-ar21.d571053a.svg"
+  },
+  {
+    "revision": "c7e753987510b15927848ab4c9ae40d3",
+    "url": "/react-portfolio/static/media/jquery-ar21.c7e75398.svg"
+  },
+  {
+    "revision": "35b0c13c1ea5ac3e912df079e86b5d4b",
+    "url": "/react-portfolio/static/media/heroku-ar21.35b0c13c.svg"
+  },
+  {
+    "revision": "aaed1372cd25de989eb8e9605c76ca73",
+    "url": "/react-portfolio/static/media/git-scm-ar21.aaed1372.svg"
+  },
+  {
+    "revision": "2f571c9e30fdb398082329aa14301174",
+    "url": "/react-portfolio/static/media/getbootstrap-ar21.2f571c9e.svg"
+  },
+  {
+    "revision": "98bc4d55f79619088f18a830e6d52df6",
+    "url": "/react-portfolio/index.html"
+  }
+];
