69e38bdc327f6e49a1155787040028e98ae15508
matt dunlop <matthewjdunlop@outlook.com>
2022-05-16 16:27:56 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.7432f0a8eaf9013b0a8f53adaf97ab19.js"
+  "/react-portfolio/precache-manifest.65a9daa91868112664bf526fcaf26bf2.js"
 );
 
 workbox.clientsClaim();
